362b3a638752817ddec22cf24daa0bcc6058b45e
Lukas Moravec <sydnexpklover@gmail.com>
2025-03-10 19:31:13 +0100

    parser work

--- fairyquery-cli/src/main.rs
+++ fairyquery-cli/src/main.rs
@@ -1,3 +1,28 @@
+mod history;
+use history::InputHistory;
+use std::io::{Write, stderr, stdin, stdout};
+
 fn main() {
-    println!("Hello, world!");
+    let mut input_history = InputHistory::new();
+    loop {
+        print!("gingerbread> ");
+        stdout().flush().unwrap();
+        // read a line from stdin
+        stdin()
+            .read_line(input_history.push())
+            .expect("Failed to read line");
+        // remove the newline character
+        let input = input_history.get();
+        // if the input is empty, skip the rest of the loop
+        if input.is_empty() {
+            continue;
+        }
+        // if the input is "exit", break the loop
+        if input == "\\q" {
+            break;
+        } else {
+            eprintln!("Unknown command: {}", input);
+            stderr().flush().unwrap();
+        }
+    }
 }
